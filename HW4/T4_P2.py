2e81c3ec6baf9fab3b9b8e9d82cd0607f746df4f
Rob Walker <robertsrwalker@gmail.com>
2022-04-02 11:07:09 -0400

    Add hw6

--- HW4/T4_P2.py
+++ HW4/T4_P2.py
@@ -4,9 +4,11 @@
 import numpy as np
 from scipy.spatial.distance import cdist
 import matplotlib.pyplot as plt
+import seaborn as sns
+import itertools
 
 # Loading datasets for K-Means and HAC
-small_dataset = np.load("P2_Autograder_Data.npy") # np.load("data/small_dataset.npy")
+small_dataset = np.load("data/small_dataset.npy")
 large_dataset = np.load("data/large_dataset.npy")
 
 np.random.seed(2)
@@ -18,23 +20,24 @@ class KMeans(object):
         self.K = K
         self.means = None
         self.runs = runs
-        self.assignments = None
+        self.assignment = None
         self.losses = np.zeros(runs)
+        self.title = "KMeans"
 
     # X is a (N x 784) array since the dimension of each image is 28x28.
     def fit(self, X):
-        # Initialize cluster assignments and random centers
-        self.assignments = np.zeros(X.shape[0])
+        # Initialize cluster assignment and random centers
+        self.assignment = np.zeros(X.shape[0])
         self.means = np.random.randn(self.K, X.shape[1])
 
         for run in range(self.runs):
             # Assign datapoints to nearest cluster
             for i, x in enumerate(X):
-                self.assignments[i] = np.argmin(np.linalg.norm(x - self.means, axis=1) ** 2)
+                self.assignment[i] = np.argmin(np.linalg.norm(x - self.means, axis=1) ** 2)
 
             # Update cluster means
             for k in range(self.K):
-                clusteroid = X[self.assignments == k]
+                clusteroid = X[self.assignment == k]
                 if clusteroid.size != 0:
                     self.means[k] = clusteroid.mean(axis=0)
             
@@ -48,62 +51,73 @@ class KMeans(object):
     
     def get_losses(self):
         return self.losses
+
+    def get_assignment(self):
+        return self.assignment
     
     def __objective(self, X):
-        return np.sum([np.linalg.norm(x - self.means[int(self.assignments[i])]) ** 2 for i, x in enumerate(X)])
+        return np.sum([np.linalg.norm(x - self.means[int(self.assignment[i])]) ** 2 for i, x in enumerate(X)])
 
 class HAC(object):
     def __init__(self, linkage):
         self.linkage = linkage
         self.assignments = []
         self.X = None
+        self.title = f"HAC with {linkage} linkage"
     
     # X is a (N x 784) array since the dimension of each image is 28x28.
     def fit(self, X):
+        # Store X
         self.X = X
 
-        # Assign each x in X to its own cluster
+        # Create initial cluster assignment
         N = X.shape[0]
-
         assignment = np.arange(N)
         self.assignments.append(np.copy(assignment))
 
-        nclusters = np.unique(assignment).size
-
+        # Perform clustering
+        nclusters = N
+        merged = set()
         while nclusters > 1:
-            if self.linkage == 'centroid':
-                # Find nearest clusters
-                midx, mval = [0, 1], float('inf')
-                for i in range(N):
-                    for j in range(i+1, N):
-                        Xi, Xj = X[assignment == i], X[assignment == j]
-                        if Xi.size == 0 or Xj.size == 0:
-                            continue
+            # Find nearest clusters
+            midx, mval = [0, 1], float('inf')
+            for i in range(N):
+                for j in range(i+1, N):
+                    if i in merged or j in merged:
+                        continue
+
+                    Xi, Xj = X[assignment == i], X[assignment == j]
+                    m = 0
+                    if self.linkage == 'centroid':
                         m = np.linalg.norm(Xi.mean(axis=0) - Xj.mean(axis=0))
-                        if m < mval:
-                            mval = m
-                            midx = [i, j]
+                    elif self.linkage == 'min':
+                        m = np.min(cdist(Xi, Xj))
+                    else:
+                        m = np.max(cdist(Xi, Xj))
 
-                # Merge clusters
-                assignment[assignment == midx[1]] = midx[0]
-                self.assignments.append(np.copy(assignment))
+                    if m < mval:
+                        mval = m
+                        midx = [i, j]
 
-                print(midx)
-                print(nclusters)
-                nclusters -= 1
-            else:
-                return
+            # Merge clusters
+            assignment[assignment == midx[1]] = midx[0]
+            merged.add(midx[1])
+            self.assignments.append(np.copy(assignment))
+            nclusters -= 1
 
         return
 
     # Returns the mean image when using n_clusters clusters
     def get_mean_images(self, n_clusters):
-        assignment = self.assignments[-n_clusters]
         means = np.zeros((n_clusters, self.X.shape[1]))
+        assignment = self.assignments[-n_clusters]
         clusters = np.unique(assignment)
         for i, cluster in enumerate(clusters):
             means[i] = self.X[assignment == cluster].mean(axis=0)
         return means
+
+    def get_assignment(self, n_clusters=10):
+        return self.assignments[-n_clusters]
 
 # Plotting code for parts 2 and 3
 def make_mean_image_plot(data, standardized=False):
@@ -120,11 +134,10 @@ def make_mean_image_plot(data, standardized=False):
         allmeans[:,i] = KMeansClassifier.get_mean_images()
         alllosses[:,i] = KMeansClassifier.get_losses()
     
-    # Plot losses
+    # Plot loss for part 1
     fig = plt.figure()
-    for i in range(niters):
-        plt.plot(alllosses[:,i], label=f'Run {i}')
-    plt.title('Losses on each run')
+    plt.plot(alllosses[:,0])
+    plt.title('K-means Loss')
     plt.xlabel('Iteration')
     plt.ylabel('Loss')
     plt.legend()
@@ -144,18 +157,21 @@ def make_mean_image_plot(data, standardized=False):
     plt.show()
 
 # ~~ Part 2 ~~
-# make_mean_image_plot(large_dataset, False)
+make_mean_image_plot(large_dataset, False)
 
 # ~~ Part 3 ~~
 # Change this line! standardize large_dataset and store the result in large_dataset_standardized
-# std = np.std(large_dataset, axis=0)
-# std[std == 0] = 1
-# large_dataset_standardized = (large_dataset - large_dataset.mean(axis=0)) / std
-# make_mean_image_plot(large_dataset_standardized, True)
+std = np.std(large_dataset, axis=0)
+std[std == 0] = 1
+large_dataset_standardized = (large_dataset - large_dataset.mean(axis=0)) / std
+np.random.seed(2)
+make_mean_image_plot(large_dataset_standardized, True)
 
 # Plotting code for part 4
-LINKAGES = ['centroid'] # [ 'max', 'min', 'centroid' ]
+LINKAGES = [ 'max', 'min', 'centroid' ]
 n_clusters = 10
+
+hacs = []
 
 fig = plt.figure(figsize=(10,10))
 plt.suptitle("HAC mean images with max, min, and centroid linkages")
@@ -164,6 +180,7 @@ for l_idx, l in enumerate(LINKAGES):
     hac = HAC(l)
     hac.fit(small_dataset)
     mean_images = hac.get_mean_images(n_clusters)
+    hacs.append(hac)
     # Make plot
     for m_idx in range(mean_images.shape[0]):
         m = mean_images[m_idx]
@@ -176,6 +193,53 @@ for l_idx, l in enumerate(LINKAGES):
         plt.imshow(m.reshape(28,28), cmap='Greys_r')
 plt.show()
 
-# TODO: Write plotting code for part 5
+# Part 4: Plot cluster sizes for each HAC linkage
+for hac in hacs:
+    _, counts = np.unique(hac.assignments[-n_clusters], return_counts=True)
+    fig = plt.figure()
+    plt.plot(counts, '.')
+    plt.title(f'HAC cluster counts with {hac.linkage} linkage')
+    plt.xlabel('Cluster index')
+    plt.ylabel('Number of images in cluster')
+    plt.show()
 
-# TODO: Write plotting code for part 6
+# Plot cluster sizes for K-means linkage
+np.random.seed(2)
+
+KMeansClassifier = KMeans(K=10, runs=10)
+KMeansClassifier.fit(small_dataset)
+fig = plt.figure()
+_, counts = np.unique(KMeansClassifier.get_assignment(), return_counts=True)
+plt.plot(counts, '.')
+plt.title(f'K-means cluster counts')
+plt.xlabel('Cluster index')
+plt.ylabel('Number of images in cluster')
+plt.show()
+
+
+# Part 5
+
+def cluster_map(arr):
+    return { v : k for k, v in dict(enumerate(arr)).items() }
+
+K = 10
+methods = [KMeansClassifier] + hacs
+for m1, m2 in itertools.combinations(methods, 2):
+    # Create confusion matrix
+    a1, a2 = m1.get_assignment(), m2.get_assignment()
+    a1m, a2m = cluster_map(np.unique(a1)), cluster_map(np.unique(a2))
+    N = a1.shape[0]
+    C = np.zeros((K, K))
+    for i in range(N):
+        C[a1m[a1[i]], a2m[a2[i]]] += 1
+
+    sns.heatmap(C)
+
+    # we can also add titles and labels
+    plt.xlabel(m1.title)
+    plt.ylabel(m2.title)
+    plt.title(f"Heatmap of {m1.title} vs {m2.title}")
+
+    # beautify, save, and show
+    plt.tight_layout()
+    plt.show()
--- HW4/test.py
+++ HW4/test.py
@@ -1,3 +1,23 @@
-for i in range(10):
-  for j in range(i+1, 10):
-    print(i, j)
+import numpy as np
+
+class A(object):
+  def assignment(self, nclusters=10):
+    return str(nclusters)
+
+class B(object):
+  def assignment(self):
+    return 'B'
+  
+methods = [A(), B()]
+for method in methods:
+  print(method.assignment())
+
+K = 10
+a1 = np.arange(10)
+a2 = np.arange(10)[::-1]
+
+N = a1.shape[0]
+C = np.zeros((K, K))
+for i in range(N):
+  C[a1[i], a2[i]] += 1
+print(C)
